a45f4d2b8d3424e10ae5a346a3e8db9942b48187
Stephen Thomas <stephen@sathomas.me>
2014-10-25 15:25:49 -0400

    clean up dependencies

--- src/scripts/scripts.04.js
+++ src/scripts/scripts.04.js
@@ -5,8 +5,9 @@
 
 //@codekit-prepend "../../bower_components/jquery/dist/jquery.js"
 //@codekit-prepend "../../bower_components/raphael/raphael.js"
-//@codekit-prepend "../../bower_components/treemap-squared/min/treemap-squared-0.5.min.js"
-//@codekit-prepend "../../bower_components/heatmap.js/src/heatmap.js"
+//@codekit-prepend "../../bower_components/treemap-squared/treemap-squarify.js"
+//@codekit-prepend "../../bower_components/treemap-squared/treemap-raphael.js"
+//@codekit-prepend "../../bower_components/heatmap/index.js"
 //@codekit-prepend "../../bower_components/sigma/build/sigma.require.js"
 //@codekit-prepend "../../bower_components/sigma/build/plugins/sigma.layout.forceAtlas2.min.js"
 //@codekit-prepend "../../bower_components/wordcloud2.js/src/wordcloud2.js"
