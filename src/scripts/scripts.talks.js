9ad2a8aa2209112ee62928202561da62042a008d
Stephen Thomas <stephen@sathomas.me>
2014-12-13 08:49:53 -0500

    continue work on talks

--- src/scripts/scripts.talks.js
+++ src/scripts/scripts.talks.js
@@ -7,9 +7,11 @@
 //@codekit-prepend "../../bower_components/d3/d3.js"
 //@codekit-prepend "../../bower_components/queue-async/queue.js"
 //@codekit-prepend "../../bower_components/crossfilter/crossfilter.js"
+//@codekit-prepend "../../bower_components/snap.svg/dist/snap.svg.js"
 
 // Component scripts
 
 //@codekit-prepend "slides.js"
 //@codekit-prepend "fonts.js"
+//@codekit-prepend "code.js"
 
